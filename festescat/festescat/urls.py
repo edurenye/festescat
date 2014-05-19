5b02501896fecce7f90f6d2ef67b06bac1349939
edurenye <edurenye@hotmail.com>
2014-05-20 01:40:14 +0200

    arreglar respostes REST

--- festescat/festescat/urls.py
+++ festescat/festescat/urls.py
@@ -21,7 +21,7 @@ urlpatterns = patterns('',
     url(r'^events/$', events, name='events'),
     url(r'^events/(\d+)/$', event),
 
-    url(r'^ubicacions/(\d+)/edit/$, ubi_update),
+    url(r'^ubicacions/(\d+)/edit/$', ubi_update),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
