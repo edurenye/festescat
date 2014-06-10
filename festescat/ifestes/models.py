aab4ef702177992634ba80e0cad962ec0dd90d34
edurenye <edurenye@hotmail.com>
2014-06-10 18:16:29 +0200

    afegides reviews i RDFa a ubicacions

--- festescat/ifestes/models.py
+++ festescat/ifestes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
 
@@ -33,6 +34,23 @@ class Festes(models.Model):
 
     def get_absolute_url(self):
         return reverse('festa_detail', kwargs={'pk': self.pk})
+
+
+class Review(models.Model):
+    RATING_CHOICES = ((1, 'one'), (2, 'two'), (3, 'three'), (4, 'four'),
+        (5, 'five'))
+    rating = models.PositiveSmallIntegerField('Rating(stars)', blank=False,
+        default=3, choices=RATING_CHOICES)
+    comment = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(User, default=User.objects.get(id=1))
+    date = models.DateField(default=date.today)
+
+    class  Meta:
+        abstract = True
+
+
+class FestaReview(Review):
+    restaurant = models.ForeignKey(Festes)
 
 
 class Events(models.Model):
--- festescat/ifestes/views.py
+++ festescat/ifestes/views.py
@@ -258,13 +258,35 @@ def festa(request, idFesta, format='html'):
                 dict(newfestaform=nff),
                 context_instance=RequestContext(request, variables))
         else:
+            mitja = 0.0
+            canAddReview = 0
+            reviews = FestaReview.objects.get(Festa=idFesta)
+            RATING_CHOICES = FestaReview.RATING_CHOICES
             organitzadors = Organitzadors.objects.all()
             org = False
+            if(len(reviews) != 0):
+                for review in reviews:
+                    mitja = mitja + review.rating
+                    if(user.id == review.user.id):
+                        canAddReview = 1
+                mitja = mitja / (len(reviews))
             if request.user in organitzadors:
                 org = True
             festa = Festes.objects.get(id=idFesta)
             if(format == 'html'):
+                variables = Context({
+                    'titlehead': 'Gestor de Festes',
+                    'pagetitle': 'Festes',
+                    'org': org,
+                    'festa': festa,
+                    'reviews': reviews,
+                    'RATING_CHOICES': RATING_CHOICES,
+                    'mitja': mitja,
+                    'canAddReview': canAddReview,
+                })
                 return HttpResponseRedirect('/festes/' + str(idFesta) + '/')
+                return render_to_response('festa.html',
+                context_instance=RequestContext(request, variables))
             else:
                 return formate(format, Festes.objects.filter(id=idFesta))
     else:
